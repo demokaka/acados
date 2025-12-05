770e3ec5bb35a334115acdf1e7c78a70d5c6f520
DINH Cong Khanh (dinhck) <dinhck@esisar.inpg.fr>
2025-12-05 15:39:57 +0100

    add corrections and modifications for Windows installation

--- interfaces/acados_matlab_octave/AcadosOcp.m
+++ interfaces/acados_matlab_octave/AcadosOcp.m
@@ -1769,7 +1769,8 @@ classdef AcadosOcp < handle
             % MEX files
             matlab_template_path = 'matlab_templates';
             template_list{end+1} = {fullfile(matlab_template_path, 'mex_solver.in.m'), [self.name, '_mex_solver.m']};
-            template_list{end+1} = {fullfile(matlab_template_path, 'make_mex.in.m'), ['make_mex_', self.name, '.m']};
+            % template_list{end+1} = {fullfile(matlab_template_path, 'make_mex.in.m'), ['make_mex_', self.name, '.m']};
+            template_list{end+1} = {fullfile(matlab_template_path, 'make_mex.in2.m'), ['make_mex_', self.name, '.m']};
             template_list{end+1} = {fullfile(matlab_template_path, 'acados_mex_create.in.c'), ['acados_mex_create_', self.name, '.c']};
             template_list{end+1} = {fullfile(matlab_template_path, 'acados_mex_free.in.c'), ['acados_mex_free_', self.name, '.c']};
             template_list{end+1} = {fullfile(matlab_template_path, 'acados_mex_solve.in.c'), ['acados_mex_solve_', self.name, '.c']};
